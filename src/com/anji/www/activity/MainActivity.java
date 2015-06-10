4f990525bd529a66bab3af45d814026e327ead16
huliang7777 <364242517@qq.com>
2015-06-10 18:01:36 +0800

    菜单优化

--- src/com/anji/www/activity/MainActivity.java
+++ src/com/anji/www/activity/MainActivity.java
@@ -484,24 +484,24 @@ public class MainActivity extends SlidingFragmentActivity implements MenuEvent
 		mSlidingMenu.setFadeDegree(0.25f);
 		mSlidingMenu.setMode(SlidingMenu.LEFT);
 //		mSlidingMenu.setBackgroundImage(R.drawable.slidingmenu_bg);
+		mSlidingMenu.setBackgroundColor( getResources().getColor(R.color.white ) );
 		
-		
-		mSlidingMenu.setBehindCanvasTransformer(new SlidingMenu.CanvasTransformer() {
-			@Override
-			public void transformCanvas(Canvas canvas, float percentOpen) {
-				float scale = (float) (percentOpen * 0.25 + 0.75);
-				canvas.scale(scale, scale, -canvas.getWidth() / 2,
-						canvas.getHeight() / 2);
-			}
-		});
+//		mSlidingMenu.setBehindCanvasTransformer(new SlidingMenu.CanvasTransformer() {
+//			@Override
+//			public void transformCanvas(Canvas canvas, float percentOpen) {
+//				float scale = (float) (percentOpen * 0.25 + 0.75);
+//				canvas.scale(scale, scale, -canvas.getWidth() / 2,
+//						canvas.getHeight() / 2);
+//			}
+//		});
 //
-		mSlidingMenu.setAboveCanvasTransformer(new SlidingMenu.CanvasTransformer() {
-			@Override
-			public void transformCanvas(Canvas canvas, float percentOpen) {
-				float scale = (float) (1 - percentOpen * 0.25);
-				canvas.scale(scale, scale, 0, canvas.getHeight() / 2);
-			}
-		});
+//		mSlidingMenu.setAboveCanvasTransformer(new SlidingMenu.CanvasTransformer() {
+//			@Override
+//			public void transformCanvas(Canvas canvas, float percentOpen) {
+//				float scale = (float) (1 - percentOpen * 0.25);
+//				canvas.scale(scale, scale, 0, canvas.getHeight() / 2);
+//			}
+//		});
 	}
 
 
